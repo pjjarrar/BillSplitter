0b61b341c1b3a9b23d74c75bf9f1233c41ff11de
phillip jarrar <pjjarrar@gmail.com>
2023-06-19 20:05:35 -0500

    Initial Commit

---
+++ splitbill/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  splitbill
+//
+//  Created by phillip jarrar on 6/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
